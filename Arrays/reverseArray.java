33fcf36a564f5e5eb5d6c371e8166212efd7fb9f
Tanushree227 <tanushreedas813@gmail.com>
2024-05-06 14:37:12 +0530

    recursive approach

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -26,18 +26,44 @@ public class reverseArray {
     }
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i=0;i<n;i++)
-            arr[i] = sc.nextInt();
-        
-        System.out.println("Given array is : ");
-        for (int i:arr)
-            System.out.print(i+" ");
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i=0;i<n;i++)
+                arr[i] = sc.nextInt();
+            
+            System.out.println("Given array is : ");
+            for (int i:arr)
+                System.out.print(i+" ");
 
-        System.out.println();
+            System.out.println();
 
-        reverseArr(arr, n);
+            reverseArr(arr, n);
+        }
     }
 }
+
+// public class Main {
+//     //Function to print array
+//     static void printArray(int arr[], int n) {
+//        System.out.print("Reversed array is:- \n");
+//        for (int i = 0; i < n; i++) {
+//           System.out.print(arr[i] + " ");
+//        }
+//     }
+//     //Function to reverse array using recursion
+//     static void reverseArray(int arr[], int start, int end) {
+//        if (start < end) {
+//           int tmp = arr[start];
+//           arr[start] = arr[end];
+//           arr[end] = tmp;
+//           reverseArray(arr, start + 1, end - 1);
+//        }
+//     }
+//     public static void main(String[] args) {
+//        int n = 5;
+//        int arr[] = { 5, 4, 3, 2, 1};
+//        reverseArray(arr, 0, n - 1);
+//        printArray(arr, n);
+//     }
+//  }
