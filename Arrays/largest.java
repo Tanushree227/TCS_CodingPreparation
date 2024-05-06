292be52634ba163cdd13f227cd9cca2d432768db
Tanushree227 <tanushreedas813@gmail.com>
2024-05-06 10:41:43 +0530

    recursive approach

--- Arrays/largest.java
+++ Arrays/largest.java
@@ -10,10 +10,21 @@ public class largest {
             arr[i] = sc.nextInt();  
         
         System.out.println("Brute Force Approach: The Largest element is : " + findLargestElement(arr));
+        System.out.println("Recursive Approach: The Largest element is : " + LargestElement(arr));
     }
     static int findLargestElement(int[] arr)
     {
         Arrays.sort(arr);
         return arr[arr.length - 1];
     }
+
+    static int LargestElement(int arr[]) {
+        int max= arr[0];
+        for (int i = 0; i < arr.length; i++) {
+          if (arr[i] > max) {
+            max= arr[i];
+          }
+        }
+        return max;
+      }
 }
