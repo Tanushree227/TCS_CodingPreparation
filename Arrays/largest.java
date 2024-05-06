6b4fd0fe5b8dc9705642ca7e63d59bca8aea531e
Tanushree227 <tanushreedas813@gmail.com>
2024-05-06 10:35:49 +0530

    Brute Force Approach

--- Arrays/largest.java
+++ Arrays/largest.java
@@ -1,4 +1,19 @@
 package Arrays;
+import java.util.*;
 public class largest {
-    
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i=0;i<n;i++)  
+            arr[i] = sc.nextInt();  
+        
+        System.out.println("Brute Force Approach: The Largest element is : " + findLargestElement(arr));
+    }
+    static int findLargestElement(int[] arr)
+    {
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+    }
 }
