4fc4b3a1007f635ba661bbdf43a32ab4a9765a84
Tanushree227 <tanushreedas813@gmail.com>
2024-05-06 14:58:00 +0530

    using maps

--- Arrays/frequencyCounter.java
+++ Arrays/frequencyCounter.java
@@ -2,38 +2,71 @@ package Arrays;
 //Problem statement: Given an array, we have found the number of occurrences of each element in the array.
 import java.util.*;
 
-public class frequencyCounter {
-    public static void main(String[] args) {
-        @SuppressWarnings("resource")
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i = 0; i < n; i++)
-        {
-            arr[i] = sc.nextInt();
-        }
-        countFreq(arr, n);
-    }
+// public class frequencyCounter {
+//     public static void main(String[] args) {
+//         @SuppressWarnings("resource")
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int arr[] = new int[n];
+//         for(int i = 0; i < n; i++)
+//         {
+//             arr[i] = sc.nextInt();
+//         }
+//         countFreq(arr, n);
+//     }
 
-    public static void countFreq(int[] arr, int n)
+//     public static void countFreq(int[] arr, int n)
+//     {
+//         boolean visited[] = new boolean[n];
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(visited[i] == true)
+//             {
+//                 continue;
+//             }
+//             int count = 1;
+//             for(int j = i + 1; j < n; j++)
+//             {
+//                 if(arr[i] == arr[j])
+//                 {
+//                     visited[j] = true;
+//                     count++;
+//                 }
+//             }
+//             System.out.println("Element " + arr[i] + " occurs " + count + " times.");
+//         }
+//     }
+// }
+
+public class frequencyCounter {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int n = scan.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scan.nextInt();
+            }
+            Frequency(arr, n);
+        }
+    }
+    static void Frequency(int arr[], int n)
     {
-        boolean visited[] = new boolean[n];
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < n; i++)
         {
-            if(visited[i] == true)
+            if(map.containsKey(arr[i]))
             {
-                continue;
+                map.put(arr[i], map.get(arr[i]) + 1);
             }
-            int count = 1;
-            for(int j = i + 1; j < n; j++)
+            else
             {
-                if(arr[i] == arr[j])
-                {
-                    visited[j] = true;
-                    count++;
-                }
+                map.put(arr[i], 1);
             }
-            System.out.println("Element " + arr[i] + " occurs " + count + " times.");
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
