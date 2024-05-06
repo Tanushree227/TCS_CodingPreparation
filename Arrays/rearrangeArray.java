a9d5fc458c940655d99f89e82a53c1309db23463
Tanushree227 <tanushreedas813@gmail.com>
2024-05-06 15:23:39 +0530

    O(nlogn)+O(n)

--- Arrays/rearrangeArray.java
+++ Arrays/rearrangeArray.java
@@ -1,1 +1,27 @@
+package Arrays;
 //Problem Statement: Rearrange the array such that the first half is arranged in increasing order, and the second half is arranged in decreasing order
+import java.util.*;
+
+public class rearrangeArray
+{
+    public static void main(String args[])
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            for(int i = 0; i < n/2; i++)
+            {
+                System.out.print(arr[i] + " ");
+            }
+            for(int i = n-1; i >= n/2; i--)
+            {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
