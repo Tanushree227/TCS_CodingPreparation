6778d9bfcf8988b8cd7d9f2a92475e7496a52fa3
Tanushree227 <tanushreedas813@gmail.com>
2024-05-07 14:44:25 +0530

    Sort Elements of an Array by Frequency

---
+++ Arrays/SortElements.java
@@ -0,0 +1,11 @@
+package Arrays;
+//Problem Statement: Given an array of integers, having some duplicate elements, sort the array by frequency.
+
+import java.util.Scanner;
+
+public class SortElements {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+        }
+    }
+}
