e4b5e93c98010db9b889449bef1871e81a6598ab
Tanushree227 <tanushreedas813@gmail.com>
2024-05-11 23:39:54 +0530

    Print all the duplicates in the string

---
+++ Strings/duplicates.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        String str = "sinstriiintng";
+
+        HashMap < Character, Integer > map = new HashMap < Character, Integer > (26);
+
+        for (int i = 0; i < str.length(); i++)
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+
+        for (Map.Entry < Character, Integer > entry: map.entrySet())
+            if (entry.getValue() > 1)
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+    }
+}
